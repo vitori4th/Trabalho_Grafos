5334e504b105ce5ecc459129d838bdc192292207
Vitória Thaís <80798968+vitori4th@users.noreply.github.com>
2022-12-18 03:16:33 -0300

    optimize: add bfs and remove todosCaminhos

--- projeto.py
+++ projeto.py
@@ -1,96 +1,112 @@
-#Em um grafo não-ponderado, o caminho máximo entre dois vértices v (origem) e u (destino) é aquele que possui a maior quantidade de arestas entre eles.
+disciplinas_semestres = {1: ['XDES01', 'SAHC04', 'SAHC05', 'MAT00A', 'IEPG01', 'IEPG22'],
+                             2: ['XDES02', 'XDES04', 'STC001', 'XMAC01', 'IEPG04'],
+                             3: ['ECN01', 'STC002', 'SRSC03', 'SDES05', 'XDES03'],
+                             4: ['SRSC02', 'XPAD01', 'SMAC03', 'XMAC02', 'IEPG14'],
+                             # 5:['ADM51E','SPAD03','SPA02','XRSC01'],
+                             # 6:['SDES06','XMC001', 'IEPG10'],
+                             # 7:['XAHC02','SDES07'],
+                             # 8:['XAHC01','XAHC03','TCC1','ADM03E'],
+                             # 9:['TCC2','SADG01']
+                             }
 
-def encontra_todos_caminhos(grafo, inicio, fim, caminho=[]):
-    caminho = caminho + [inicio]
-    if inicio == fim:
-        return [caminho]
-    if inicio not in grafo:
-        return []
-    caminhos = []
-    for no in grafo[inicio]:
-        if no not in caminho:
-            novos_caminhos = encontra_todos_caminhos(grafo, no, fim, caminho)
-            for novo_caminho in novos_caminhos:
-                caminhos.append(novo_caminho)
-    return caminhos
+def bfs(grafo, v):
+    lista=[]
+    q=[]
+    q.append(v)
+    while q!=[]:
+        v=q.pop()
+        for adjacente in grafo[v]:
+            if adjacente not in q:
+                q.append(adjacente)
+                continue
+        lista.append(v)
+    return lista
 
-def monta_grade(historico_disciplinas, periodo,disciplina_falta_fazer=[] ):
-    disciplinas_fazer =[] #disciplinas que o aluno irá fazer esse semestre
-    disciplinas_impossivel_fazer=[] #disciplinas que ele não pode fazer
-    disciplinas_mais_importantes=[] # disciplinas mais importantes (críticas) serão feitas no semestre que ele está
+def monta_grade(historico_disciplinas, periodo, numero, disciplina_falta_fazer=[]):
     # dicionario com periodo(chave) e disciplinas (valor)
-    disciplinas_semestres ={ 1:['A','B','C'], 2:['D','E','F'],3:['G','H','I'] }
-    #quantidade de disciplinas que ele pega no semestre em que ele está
-    quantidade_disciplinas_periodo = len(disciplinas_semestres[periodo])
-    #Grafo com as relações de pré requisito das disciplinas
-    grafo ={'A': ['G'],
-        'B': ['E'],
-        'C': ['F'],
-        'D': ['G'],
-         'E': ['H'],
-         'F': ['I'],
-        'G': ['fim'],
-         'H': ['fim'],
-         'I': ['fim'],
-            'fim':[]}
-    grafo1 = {'A': ['fim'],
-             'B': ['D'],
-             'C': ['fim'],
-             'D': ['fim'],
-             'E': ['fim'],
-             'F': ['fim'],
-             'G': ['fim'],
-             'H': ['fim'],
-             'I': ['fim'],
-            'fim':[]}
+    if historico_disciplinas==[]:
+        #se aluno acabou de começar retorna o padrão
+        return print(disciplinas_semestres)
+
+    disciplinas_pode_fazer = []  # disciplinas que o aluno pode fazer esse semestre
+    disciplinas_impossivel_fazer = []  # disciplinas que ele não pode fazer (por conta de préq requisitos ou questão de oferta
+    disciplinas_mais_importantes = []  # disciplinas mais importantes (críticas) serem feitas no semestre que ele está
+
+    # verifica se o periodo é par ou impar, para definir quais disciplinas ele pode ou não pegar
+    periodo_impar = True
+    if numero % 2 == 0:
+        periodo_impar = False
+
+    # quantidade de disciplinas que ele irá pegar
+    if len(disciplinas_semestres[periodo]) < 4:
+        quantidade_disciplinas_periodo = 4
+    else:
+        quantidade_disciplinas_periodo = len(disciplinas_semestres[periodo])
+
+    # Grafo com as relações de pré requisito das disciplinas
+    grafo = {'XDES01': ['XDES02'], 'SAHC04': ['XDES02'], 'SAHC05': ['XDES03','SDES05'], 'MAT00A': ['XDES02'],'IEPG01': ['fim'],'IEPG22': ['fim'],
+            'XDES02': ['fim'],'XDES04': ['fim'],'STC001': ['fim'],'XMAC01': ['fim'],'IEPG04': ['fim'],'ECN01': ['fim'], 'STC002': ['fim'], 'SRSC03': ['fim'], 'SDES05': ['fim'], 'XDES03': ['fim'],
+             'SRSC02': ['fim'], 'XPAD01': ['fim'], 'SMAC03': ['fim'], 'XMAC02': ['fim'], 'IEPG14': ['fim'],'fim':[]
+             }
 
     # verifica quais disciplinas dos semestres anteriores ele deixou de fazer e quais as disciplinas do semestre q ele está
-    for posicao in range(1,periodo+1):
-        # para cada disciplinas já cursada
+    for posicao in range(1, periodo + 1):
+        # para cada disciplina já cursada
         for disciplina in historico_disciplinas:
             if disciplina in disciplinas_semestres[posicao]:
                 # remove a disciplina do semestre tal
                 disciplinas_semestres[posicao].remove(disciplina)
         # adiciona as disciplinas que restaram no semestre x à lista de disciplinas faltantes
-        disciplina_falta_fazer= disciplina_falta_fazer + disciplinas_semestres[posicao]
-    disciplina_falta_fazer = list(set(disciplina_falta_fazer)) # remove duplicatas
+        disciplina_falta_fazer = disciplina_falta_fazer + disciplinas_semestres[posicao]
+    disciplina_falta_fazer = list(set(disciplina_falta_fazer))  # remove duplicatas
 
-    # para cada disciplina do grafo será verificado sua relação com as disciplinas já feitas pelo aluno e as faltantes do semestre
+    # para cada disciplina do grafo será verificado sua relação com as disciplinas já feitas pelo aluno e as faltantes do semestre através de uma bfs
     for disciplina in disciplina_falta_fazer:
-        caminhos = encontra_todos_caminhos(grafo, disciplina,'fim')
-        # para cada materia retornada pela BFS da disciplina do grafo será analisada se o aluno pode/precisa ou não fazer
-        for caminho in caminhos:
-            for x in range(1, len(caminho)-1):
-                if caminho[x] not in disciplinas_impossivel_fazer:
-                    disciplinas_impossivel_fazer.append(caminho[x])
-            if disciplina not in disciplinas_impossivel_fazer:
-                if disciplina not in disciplinas_fazer:
-                    disciplinas_fazer.append(disciplina)
+        resultado_bfs = bfs(grafo, disciplina)
+        for materia in resultado_bfs:
+            if materia == disciplina:
+                continue
+            if materia not in disciplinas_impossivel_fazer:
+                disciplinas_impossivel_fazer.append(materia)
+        if disciplina not in disciplinas_impossivel_fazer:
+            if disciplina not in disciplinas_pode_fazer:
+                disciplinas_pode_fazer.append(disciplina)
 
-    #tira as disciplinas que ele não consegue fazer da lista de disciplinas que ele pode pegar para fazer (disciplinas_fazer)
+    # remove as disciplinas que ele não consegue fazer por conta dos pré requisitos
     for x in disciplinas_impossivel_fazer:
-        if x in disciplinas_fazer:
-            disciplinas_fazer.remove(x)
+        if x in disciplinas_pode_fazer:
+            disciplinas_pode_fazer.remove(x)
 
-    #para cada disciplinas das que ele pode fazer
-    for disciplina in disciplinas_fazer:
-        if len(disciplinas_mais_importantes)== quantidade_disciplinas_periodo:
+    # tira as disciplinas que não são ofertadas naquele semestre, da lista disciplinas_pode_fazer
+    for tam in range(1, len(disciplinas_semestres)):
+        # verifica em qual semestre está a disciplina
+        teste = tam % 2 != 0
+        if teste == periodo_impar:
+            continue
+        for disciplina in disciplinas_pode_fazer:
+            if disciplina in disciplinas_semestres[tam]:
+                disciplinas_pode_fazer.remove(disciplina)
+
+    #Definindo prioridade das disciplinas de acordo com a bfs
+    for disciplina in disciplinas_pode_fazer:
+        if len(disciplinas_mais_importantes) == quantidade_disciplinas_periodo:
             break
-        caminhos = encontra_todos_caminhos(grafo, disciplina, 'fim')
-        for caminho in caminhos:
-            if caminho[1] !='fim':
-                if periodo+1 in disciplinas_semestres:
-                    if caminho[1] in disciplinas_semestres[periodo+1]:
-                        #adiciona na lista de disciplinas críticas
-                        disciplinas_mais_importantes.append(disciplina)
-
-                if periodo+2 in disciplinas_semestres:
-                    if caminho[1] in disciplinas_semestres[periodo + 2]:
+        result = bfs(grafo,disciplina)
+        for materia in result:
+            if materia != 'fim':
+                if periodo + 1 in disciplinas_semestres:
+                    if materia in disciplinas_semestres[periodo + 1]:
                         # adiciona na lista de disciplinas críticas
-                        disciplinas_mais_importantes.append(disciplina)
+                        if disciplina not in disciplinas_mais_importantes:
+                            disciplinas_mais_importantes.append(disciplina)
+                if periodo + 2 in disciplinas_semestres:
+                    if materia in disciplinas_semestres[periodo + 2]:
+                        # adiciona na lista de disciplinas críticas
+                        if disciplina not in disciplinas_mais_importantes:
+                            disciplinas_mais_importantes.append(disciplina)
 
-    #completa a lista de disciplinas criticas com o restante das disciplinas
-    for disciplina in disciplinas_fazer:
+    # completa a lista de disciplinas criticas com o restante das disciplinas
+    for disciplina in disciplinas_pode_fazer:
         if len(disciplinas_mais_importantes) == quantidade_disciplinas_periodo:
             break
         if disciplina not in disciplinas_mais_importantes:
@@ -103,15 +119,16 @@ def monta_grade(historico_disciplinas, periodo,disciplina_falta_fazer=[] ):
     for disciplina in disciplinas_mais_importantes:
         disciplina_falta_fazer.remove(disciplina)
 
-    print('semestre:',periodo,disciplinas_mais_importantes)
+    print('semestre:', numero, disciplinas_mais_importantes)
 
-    #irá chamar novamente a função de montar grade, até que todos os periodos da lista dos semestres sejam analisados e não falta nenhuma disciplina
-    if periodo <len(disciplinas_semestres):
-        monta_grade(historico_disciplinas,periodo+1,disciplina_falta_fazer)
+    # irá chamar novamente a função de montar grade, até que todos os periodos da lista dos semestres sejam analisados e não fale nenhuma disciplina
+    if periodo < len(disciplinas_semestres):
+        monta_grade(historico_disciplinas, periodo + 1, numero + 1, disciplina_falta_fazer)
     else:
-        if len(disciplina_falta_fazer) !=0:
-            monta_grade(historico_disciplinas, periodo, disciplina_falta_fazer)
+        if len(disciplina_falta_fazer) != 0:
+            monta_grade(historico_disciplinas, periodo, numero + 1, disciplina_falta_fazer)
 
-#disicplinas que o aluno ja fez
-grafo = []
-monta_grade(grafo, 1)
+
+# disicplinas que o aluno ja fez
+grafo = ['IEPG01']
+monta_grade(grafo, 1, 1)
